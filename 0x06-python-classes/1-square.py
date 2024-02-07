64fbf0f0bcaceee8eda88c8fa91e0c2f973e24db
netto-byte <marvellousmatthew@gmail.com>
2024-02-07 15:04:49 -0800

    Declares a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class."""
+
+
+class Square:
+    """A Square class."""
+
+    def __init__(self, size):
+        """Initializes an instance of Square."""
+        self.__size = None
