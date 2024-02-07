1b0bbd7d2af57685b8f49db70bd676879cedf633
netto-byte <marvellousmatthew@gmail.com>
2024-02-07 15:56:37 -0800

    Added a new private instance and modified my_strin

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,7 +32,7 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(n, int) for n in value) or
+                not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integrs")
         self.__position = value
@@ -45,9 +45,10 @@ class Square:
         """Prints in stdout the square with the character '#'."""
         if self.__size == 0:
             print()
-        else:
-            [print() for a in range(self.__position[1])]
-            for i in range(self.__size):
-                [print(' ', end='') for j in range(self.__position[0])]
-                [print('#', end='') for k in range(self.__size)]
-                print()
+            return
+
+        [print() for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(' ', end='') for j in range(0, self.__position[0])]
+            [print('#', end='') for k in range(0, self.__size)]
+            print()
